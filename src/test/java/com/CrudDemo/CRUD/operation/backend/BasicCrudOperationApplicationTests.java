7817abfdaa93dc3252c11325b4bbff9c6e70d719
fmbackenduser <muthukannan@nayamsoft.com>
2023-11-20 18:23:27 +0530

    First commit with all code

---
+++ src/test/java/com/CrudDemo/CRUD/operation/backend/BasicCrudOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CrudDemo.CRUD.operation.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicCrudOperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
