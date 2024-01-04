46bbc77af9952c93e5cb4c18f39c0a5fb1663643
MuthukannanK56 <inbox2muthu@gmail.com>
2024-01-04 18:14:35 +0530

    getStudentById and UpdateStudentById Api added

--- src/main/java/com/CrudDemo/CRUD/operation/backend/Controller/StudentController.java
+++ src/main/java/com/CrudDemo/CRUD/operation/backend/Controller/StudentController.java
@@ -1,8 +1,10 @@
 package com.CrudDemo.CRUD.operation.backend.Controller;
 
+import com.CrudDemo.CRUD.operation.backend.Exception.*;
 import com.CrudDemo.CRUD.operation.backend.Model.*;
 import com.CrudDemo.CRUD.operation.backend.Repository.*;
 import org.springframework.beans.factory.annotation.*;
+import org.springframework.http.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,7 +16,6 @@ import java.util.*;
 public class StudentController {
     @Autowired
     private StudentRepository studentRepository;
-
 
     //Get all student list
     @GetMapping("/student")
@@ -28,4 +29,37 @@ public class StudentController {
     public Student createStudent(@RequestBody Student student){
         return studentRepository.save(student);
     }
+
+    //Get Student By ID
+    @GetMapping("/student/{id}")
+    public ResponseEntity<Student> getStudentbyid(@PathVariable Long id){
+        Student student= studentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found with this id"+id));
+        return ResponseEntity.ok(student);
+    }
+
+    //Update Student By Id
+    @PutMapping("/updatestudent/{id}")
+    public ResponseEntity<Student> updateEmployee(@PathVariable Long id, @RequestBody Student studentDetails){
+        //studentDetails  is object it is input from user for update
+
+        //Student model object -> student
+        Student student = studentRepository.findById(id)
+                .orElseThrow(()->new ResourceNotFoundException("Employee not found with this id"+id));
+        //student-> model object
+        //studentDetails -> input from user
+        // set the new data(studentDetails.getName()) to table (student.setName)
+        student.setName(studentDetails.getName());
+        student.setMobilenumber(studentDetails.getMobilenumber());
+        student.setAddress(studentDetails.getAddress());
+        student.setEmail(studentDetails.getEmail());
+
+        Student updatedstudent = studentRepository.save(student);
+        return ResponseEntity.ok(updatedstudent);
+
+    }
+
+
+
+
 }
